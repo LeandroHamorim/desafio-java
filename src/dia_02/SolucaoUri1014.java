d323748e5d7b659b2ccd7c1717d1bfe7a88ee413
Leandro Henrique <leandrohamorim@outlook.com>
2021-07-22 00:30:46 -0300

    Dia 02

--- src/dia_02/SolucaoUri1014.java
+++ src/dia_02/SolucaoUri1014.java
@@ -1,9 +1,17 @@
 package dia_02;
 
+import java.util.Scanner;
+
 public class SolucaoUri1014 {
 
 	public static void main(String[] args) {
+		var sc = new Scanner(System.in);
+		int distanciaPecorrida = sc.nextInt();
+		double combustivelGasto = sc.nextDouble();
 		
+		double consumoMedio = distanciaPecorrida / combustivelGasto;
+		System.out.println(String.format("%.3f", consumoMedio)+" km/l");
+		sc.close();
 		
 	}
 
